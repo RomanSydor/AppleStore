fe4379c6ab657b3da7e32f3b53a26fdde2400158
Roman <first.luker@gmail.com>
2020-07-27 11:38:52 +0300

    Added amount validation and refactored adding of the similar Macs in the cart.

--- AppleStore/Services/MacService.cs
+++ AppleStore/Services/MacService.cs
@@ -16,23 +16,44 @@ namespace AppleStore.Services
 
         public void AddToCart(int id, int amount)
         {
+            Product existMac = null;
             var mac = _dataContext.Macs
                 .SingleOrDefault(i => i.Id == id);
 
-            var cartItem = new Product();
-            cartItem.ProductId = mac.Id;
-            cartItem.TableName = "Macs";
-            cartItem.ProductName = mac.MacModel;
-            cartItem.Color = mac.Color;
-            cartItem.ScreenSize = mac.ScreenSize;
-            cartItem.Memory = mac.Memory;
-            cartItem.Amount = amount;
-            cartItem.Price = mac.Price;
-            cartItem.TotalPrice += amount * mac.Price;
+            foreach (var item in _cart.CartList)
+            {
+                if (item.ProductId == mac.Id && item.TableName == "Macs")
+                {
+                    existMac = _cart.CartList
+                        .SingleOrDefault(i => i.ProductId == item.ProductId
+                        && i.TableName == item.TableName);
+                }
+            }
 
-            _cart.CartList.Add(cartItem);
+            if (existMac != null)
+            {
+                existMac.Amount += amount;
+                existMac.TotalPrice += amount * existMac.Price;
+                mac.AmountOfProduct -= amount;
+            }
+            else
+            {
+                var cartItem = new Product();
+                cartItem.ProductId = mac.Id;
+                cartItem.TableName = "Macs";
+                cartItem.ProductName = mac.MacModel;
+                cartItem.Color = mac.Color;
+                cartItem.ScreenSize = mac.ScreenSize;
+                cartItem.Memory = mac.Memory;
+                cartItem.Amount = amount;
+                cartItem.Price = mac.Price;
+                cartItem.TotalPrice += amount * mac.Price;
 
-            mac.AmountOfProduct -= amount;
+                _cart.CartList.Add(cartItem);
+
+                mac.AmountOfProduct -= amount;
+            }
+
             _dataContext.SaveChanges();
         }
 
