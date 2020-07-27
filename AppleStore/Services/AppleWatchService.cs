a18de9a3341d03fc5f62e0fa9f3c4345e352e492
Roman <first.luker@gmail.com>
2020-07-27 12:14:56 +0300

    Added amount validation and refactored adding of the similar AppleWatch in the cart.

--- AppleStore/Services/AppleWatchService.cs
+++ AppleStore/Services/AppleWatchService.cs
@@ -16,22 +16,43 @@ namespace AppleStore.Services
 
         public void AddToCart(int id, int amount)
         {
+            Product existWatch = null;
             var watch = _dataContext.AppleWatches
                 .SingleOrDefault(i => i.Id == id);
 
-            var cartItem = new Product();
-            cartItem.ProductId = watch.Id;
-            cartItem.TableName = "AppleWatches";
-            cartItem.ProductName = watch.AppleWatchModel;
-            cartItem.Color = watch.Color;
-            cartItem.ScreenSize = watch.ScreenSize;
-            cartItem.Amount = amount;
-            cartItem.Price = watch.Price;
-            cartItem.TotalPrice += amount * watch.Price;
+            foreach (var item in _cart.CartList)
+            {
+                if (item.ProductId == watch.Id && item.TableName == "AppleWatches")
+                {
+                    existWatch = _cart.CartList
+                        .SingleOrDefault(i => i.ProductId == item.ProductId
+                        && i.TableName == item.TableName);
+                }
+            }
 
-            _cart.CartList.Add(cartItem);
+            if (existWatch != null)
+            {
+                existWatch.Amount += amount;
+                existWatch.TotalPrice += amount * existWatch.Price;
+                watch.AmountOfProduct -= amount;
+            }
+            else
+            {
+                var cartItem = new Product();
+                cartItem.ProductId = watch.Id;
+                cartItem.TableName = "AppleWatches";
+                cartItem.ProductName = watch.AppleWatchModel;
+                cartItem.Color = watch.Color;
+                cartItem.ScreenSize = watch.ScreenSize;
+                cartItem.Amount = amount;
+                cartItem.Price = watch.Price;
+                cartItem.TotalPrice += amount * watch.Price;
 
-            watch.AmountOfProduct -= amount;
+                _cart.CartList.Add(cartItem);
+
+                watch.AmountOfProduct -= amount;
+            }
+
             _dataContext.SaveChanges();
         }
 
